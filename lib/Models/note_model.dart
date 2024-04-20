2935f17083cfedd39c17983c69e514b41311c823
Samah <Samahosama112@icloud.com>
2024-04-20 13:19:08 +0200

    creat note model

---
+++ lib/Models/note_model.dart
@@ -0,0 +1,11 @@
+class NoteModel {
+  final String title;
+  final String body;
+  final String date;
+  final int color;
+  NoteModel(
+      {required this.title,
+      required this.body,
+      required this.date,
+      required this.color});
+}
