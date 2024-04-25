16cbc7cb2897c4fa8fe9c53d31826161084b6f19
Samah <Samahosama112@icloud.com>
2024-04-25 23:55:45 +0200

    creat fetch note method and provide notes cubit

--- lib/cubits/note_cubit/notes_cubit.dart
+++ lib/cubits/note_cubit/notes_cubit.dart
@@ -1,11 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
 import 'package:notes_app/Models/note_model.dart';
+import 'package:notes_app/constant.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
 
-
+  fetchAllNotes() {
+    try {
+      var notes = Hive.box<NoteModel>(kNoteBox);
+      List<NoteModel> noteList = notes.values.toList();
+      emit(NotesSuccess(notes: noteList));
+    } catch (e) {
+      emit(NotesFailure(errMessage: e.toString()));
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,13 @@ import 'package:notes_app/Models/note_model.dart';
 import 'package:notes_app/Views/edit_note_view.dart';
 import 'package:notes_app/Views/notes_view.dart';
 import 'package:notes_app/constant.dart';
+import 'package:notes_app/cubits/note_cubit/notes_cubit.dart';
 import 'package:notes_app/simple_bloc_observer.dart';
 
 void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(NoteModelAdapter());
   await Hive.openBox<NoteModel>(kNoteBox);
-  
 
   //here i'm telling Hive that u have notemodel that u need to deal with through NoteModelAdapter
   Bloc.observer = SimpleBlocObserver();
@@ -22,17 +22,24 @@ class NoteApp extends StatelessWidget {
   const NoteApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        NotesView.id: (context) => const NotesView(),
-        EditNotesView.id: (context) => const EditNotesView()
-      },
-      initialRoute: NotesView.id,
-      theme: ThemeData(
-          fontFamily: 'Poppins',
-          brightness: Brightness.dark,
-          scaffoldBackgroundColor: const Color(0xFF303030)),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => NotesCubit(),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          NotesView.id: (context) => const NotesView(),
+          EditNotesView.id: (context) => const EditNotesView()
+        },
+        initialRoute: NotesView.id,
+        theme: ThemeData(
+            fontFamily: 'Poppins',
+            brightness: Brightness.dark,
+            scaffoldBackgroundColor: const Color(0xFF303030)),
+      ),
     );
   }
 }
