9a869da7f60ba9c86ca2605f42b41558ae938cb8
Samah <Samahosama112@icloud.com>
2024-04-30 13:26:28 +0300

    colors list view part 2

--- lib/Views/Widgets/custom_colors_list.dart
+++ lib/Views/Widgets/custom_colors_list.dart
@@ -1,27 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
+  const ColorItem({super.key, required this.isSelected, required this.colors});
+  final bool isSelected;
+  final Color colors;
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(
-      backgroundColor: Colors.blue,
-      radius: 30,
-    );
+    return isSelected
+        ? CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 30,
+            child: CircleAvatar(
+              backgroundColor: colors,
+              radius: 25,
+            ),
+          )
+        : CircleAvatar(
+            backgroundColor: colors,
+            radius: 30,
+          );
   }
 }
 
-class ColorsListView extends StatelessWidget {
+class ColorsListView extends StatefulWidget {
   const ColorsListView({super.key});
+
   @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  @override
+  int currentIndex = 0;
+  List<Color> colorsList = const [
+    Color(0xffE63946),
+    Color(0xFFFFD54F),
+    Color(0xffA8DADC),
+    Color(0xff1D3557),
+    Colors.black,
+  ];
+
   Widget build(BuildContext context) {
     return SizedBox(
       height: 60,
       child: ListView.builder(
           scrollDirection: Axis.horizontal,
-          itemCount: 10,
-          itemBuilder: (context, int) {
-            return  const ColorItem();
+          itemCount: colorsList.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: GestureDetector(
+                onTap: () {
+                  currentIndex = index;
+                  setState(() {});
+                },
+                child: ColorItem(
+                  isSelected: currentIndex == index,
+                  colors: colorsList[index],
+                ),
+              ),
+            );
           }),
     );
   }
